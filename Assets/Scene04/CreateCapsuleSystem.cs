f04bcacf4a444653ab32dc935285b06092125db1
leo <medina.engineering@outlook.com>
2021-01-23 12:11:33 -0300

    Commit03

--- Assets/Scene04/CreateCapsuleSystem.cs
+++ Assets/Scene04/CreateCapsuleSystem.cs
@@ -14,17 +14,25 @@ public class CreateCapsuleSystem : JobComponentSystem
     {
         base.OnCreate();
 
-        var instance = EntityManager.CreateEntity(ComponentType.ReadOnly<LocalToWorld>(), ComponentType.ReadOnly<RenderMesh>());/*(ComponentType.ReadWrite<Translation>(), ComponentType.ReadWrite<Rotation>(), ComponentType.ReadOnly<RenderMesh>());*/
+        var instance = EntityManager.CreateEntity(ComponentType.ReadOnly<LocalToWorld>(), ComponentType.ReadOnly<RenderMesh>(),
+            ComponentType.ReadOnly<RenderBounds>(), ComponentType.ReadOnly<WorldRenderBounds>(), ComponentType.ReadOnly<NonUniformScale>());/*(ComponentType.ReadWrite<Translation>(), ComponentType.ReadWrite<Rotation>(), ComponentType.ReadOnly<RenderMesh>());*/
 
 
-        EntityManager.SetComponentData(instance, 
-            new LocalToWorld 
-            { 
+        EntityManager.SetComponentData(instance,
+            new LocalToWorld
+            {
                 Value = new float4x4(rotation: quaternion.identity, translation: new float3(0, 0, 0))
             });
 
         //EntityManager.SetComponentData(instance, new Translation { Value = new float3(0, 0, 0) });
         //EntityManager.SetComponentData(instance, new Rotation { Value = new quaternion(0, 0, 0, 0) });
+
+        EntityManager.SetComponentData(instance, 
+            new NonUniformScale
+            { 
+                Value = new float3(10, 10, 10)
+            });
+
 
 
         var resourceHolder = Resources.Load<GameObject>("ResourceHolder").GetComponent<ResourceHolder>();
